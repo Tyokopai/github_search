3943f3c93d627bb8cedcb730b363881a884b8c4e
Tyokopai <take353@gmail.com>
2018-07-21 21:28:45 +0900

    search box and style

--- container/Detail.js
+++ container/Detail.js
@@ -1,4 +1,50 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
-export default ({ navigation }) => console.log(navigation) ||  <Text>{navigation.state.params.item.name}</Text>;
+export default class Detail extends React.Component {
+
+    render() {
+        const { navigation: { state: { params: { item } } } } = this.props;
+        console.log(this.props);
+        return (
+            <View style={styles.container}>
+                <Text style={styles.fullName}>{item.full_name}</Text>
+                <View style={styles.userBlock}>
+                    <Image style={styles.ownerIcon} source={{ uri: item.owner.avatar_url }} />
+                    <Text style={styles.ownerName}>{item.owner.login}</Text>
+                </View>
+                <Text>{item.description}</Text>
+                <Text style={styles.repoUrl}>{item.url}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        backgroundColor: '#FFFFFF',
+    },
+    fullName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    ownerIcon: {
+        width: 20,
+        height: 20,
+        borderRadius: 10,
+        marginRight: 5,
+    },
+    ownerName: {
+        fontSize: 14,
+    },
+    userBlock: {
+        flexDirection: 'row',
+    },
+    repoUrl: {
+        marginTop: 10,
+        marginBottom: 10,
+        color: '#3d3d3d',
+    },
+});
--- container/Home.js
+++ container/Home.js
@@ -1,35 +1,38 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
   FlatList,
+  TextInput,
+  Image,
 } from 'react-native';
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component<Props> {
 
   state = {
     items: [],
     refreshing: false,
+    text: '',
   }
 
   initPage = 0;
 
-  fetchRepositories(query='react', refreshing = false) {
+  fetchRepositories(refreshing = false) {
 
     const newPage = refreshing ? true : this.initPage + 1;
-    
-    fetch(`https://api.github.com/search/repositories?q=${query}&page=${newPage}`)
+
+    fetch(`https://api.github.com/search/repositories?q=${this.state.text}&page=${newPage}`)
       .then(response => response.json())
-      .then(({items}) => {
+      .then(({ items }) => {
         this.initPage = newPage;
-        if (this.refreshing) {
-          this.setState({items: items, refreshing : false});
+        if (refreshing) {
+          this.setState({ items: items, refreshing: false });
         } else {
           // 以前のページの検索結果を連結
-          this.setState({items: [...this.state.items, ...items], refreshing: false});
+          this.setState({ items: [...this.state.items, ...items], refreshing: false });
         }
       });
   }
@@ -41,20 +44,27 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={() => this.fetchRepositories()}>
-          <Text style={{marginTop: 20}}>fetch</Text>
-        </TouchableOpacity>
-        <FlatList 
+        <View style={styles.inputWrapper}>
+          <TextInput style={styles.input} onChangeText={(text) => this.setState({ text })} />
+          <TouchableOpacity onPress={() => this.fetchRepositories(true)}>
+            <Text style={styles.searchButton}>Search</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
           data={this.state.items}
-          renderItem={({item}) => 
-            <TouchableOpacity onPress={() => this.navigationDetail(item)}>
-              <Text style={{padding: 20}} >{item.name}</Text>
+          renderItem={({ item }) =>
+            <TouchableOpacity style={{ padding: 10 }} onPress={() => this.navigationDetail(item)}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }} >{item.name}</Text>
+              <View style={styles.userBlock}>
+                <Image style={styles.ownerIcon} source={{ uri: item.owner.avatar_url }} />
+                <Text style={styles.ownerName}>{item.owner.login}</Text>
+              </View>
             </TouchableOpacity>
           }
           keyExtractor={(item) => item.id.toString()}
           onEndReached={() => this.fetchRepositories()}
           onEndReachedThreshold={0.1}
-          onRefresh={() => {this.fetchRepositories(true)}}
+          onRefresh={() => { this.fetchRepositories(true) }}
           refreshing={this.state.refreshing}
         />
       </View>
@@ -65,8 +75,35 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    padding: 20,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#eeeeee',
+    borderRadius: 4,
+  },
+  searchButton: {
+    padding: 10,
+    marginLeft: 6,
+    backgroundColor: '#2796dd',
+  },
+  ownerIcon: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    marginRight: 5,
+  },
+  ownerName: {
+    fontSize: 14,
+  },
+  userBlock: {
+    flexDirection: 'row',
   },
 });
